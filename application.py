76b2b67f8d9cfcfc66db48db38bcd38c3b6390c9
Silvia Piol <Silvia Piol>
2016-07-21 14:16:56 +0100

    fix fb login and delete cascade

--- application.py
+++ application.py
@@ -478,5 +478,5 @@ def disconnect():
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
+    app.debug = False
     app.run(host='0.0.0.0', port=8000)
--- db_setup.py
+++ db_setup.py
@@ -21,7 +21,7 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship(User, cascade="delete")
 
     @property
     def serialize(self):
@@ -40,9 +40,9 @@ class Item(Base):
     description = Column(String(250))
     price = Column(String(8))
     category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category)
+    category = relationship(Category, cascade="delete")
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship(User, cascade="delete")
 
     @property
     def serialize(self):
