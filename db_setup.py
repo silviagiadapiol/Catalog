3b8973bfbf1d8f1388aa7dcd2253061e6eef8737
Silvia Piol <Silvia Piol>
2016-08-01 15:11:20 +0100

    cascade setup

--- db_setup.py
+++ db_setup.py
@@ -21,7 +21,7 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User, cascade="delete")
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -40,9 +40,9 @@ class Item(Base):
     description = Column(String(250))
     price = Column(String(8))
     category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category, cascade="delete")
+    category = relationship(Category)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User, cascade="delete")
+    user = relationship(User)
 
     @property
     def serialize(self):
